794bdc1a792440045a95ae97d148184241fc9864
bnuhero <liuquanbo@gmail.com>
2019-08-23 17:39:53 +0800

    stop instead of killing the services

--- scripts/stop_all_services.sh
+++ scripts/stop_all_services.sh
@@ -4,7 +4,7 @@
 killall ss-redir 2>/dev/null
 killall ss-local 2>/dev/null
 killall v2ray-plugin 2>/dev/null
-/opt/etc/init.d/S02haveged kill
-/opt/etc/init.d/S09dnscrypt-proxy2 kill
+/opt/etc/init.d/S02haveged stop
+/opt/etc/init.d/S09dnscrypt-proxy2 stop
 
 echo "All service stopped."
