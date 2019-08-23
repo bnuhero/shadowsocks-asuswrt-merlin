ac4b10747e5a68c3f1ca0d5eb6c191702e0695c6
bnuhero <liuquanbo@gmail.com>
2019-08-23 17:10:30 +0800

    add ss-redir service

--- scripts/start_all_services.sh
+++ scripts/start_all_services.sh
@@ -2,6 +2,14 @@
 
 SS_MERLIN_HOME=/opt/share/ss-merlin
 
+ss_pid=$(pidof ss-redir)
+if [[ -z "$ss_pid" ]]; then
+  if [[ ! -f ${SS_MERLIN_HOME}/etc/shadowsocks/config.json ]]; then
+    cp ${SS_MERLIN_HOME}/etc/shadowsocks/config.sample.json ${SS_MERLIN_HOME}/etc/shadowsocks/config.json
+  fi
+  ss-redir -c ${SS_MERLIN_HOME}/etc/shadowsocks/config.json -f /opt/var/run/ss-redir.pid
+fi
+
 local_pid=$(pidof ss-local)
 if [[ -z "$local_pid" ]]; then
   if [[ ! -f ${SS_MERLIN_HOME}/etc/shadowsocks/local.json ]]; then
--- scripts/stop_all_services.sh
+++ scripts/stop_all_services.sh
@@ -2,6 +2,7 @@
 
 # Kill all services
 killall ss-redir 2>/dev/null
+killall ss-local 2>/dev/null
 killall v2ray-plugin 2>/dev/null
 killall https_dns_proxy 2>/dev/null
 
